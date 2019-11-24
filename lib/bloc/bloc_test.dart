de5727c23326f2fd5e61e7526266bb0ab1808378
thanh23101998 <ninjacuibap@gmail.com>
2019-11-24 23:26:25 +0700

    first_commit

---
+++ lib/bloc/bloc_test.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:flutter_bloc/provider/bloc_provider.dart';
+
+class MyBloc extends BlocBase{
+  int _counter;
+  StreamController<int> streamListController = StreamController<int>.broadcast();
+  @override
+  void dispose() {
+    streamListController.close();
+    actionController.close();
+  }
+  Sink<int> get AddTest => streamListController.sink;
+  Stream<int> get GetTest => streamListController.stream;
+  StreamController actionController = StreamController();
+  Sink get IncretmentConter => actionController.sink;
+  MyBloc(){
+    _counter = 0;
+    actionController.stream.listen(_handleLogic);
+  }
+  void _handleLogic(data){
+    _counter = _counter+1;
+    AddTest.add(_counter);
+  }
+
+}
